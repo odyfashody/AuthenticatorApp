e1975d6d5a2b058d1dc909fd4e8f72f4c775c6a9
Nathanael Garza <nate.garza@hotmail.com>
2020-04-18 01:26:56 -0700

    added bookApptActivity & mainActivity

--- app/src/main/java/com/example/authenticatorapp/MainActivity.java
+++ app/src/main/java/com/example/authenticatorapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.authenticatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonBookAppt = (Button)findViewById(R.id.buttonBookAppt);
+        Button buttonBusinessLogin = (Button)findViewById(R.id.buttonBusinessLogin);
+
+        buttonBookAppt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goPage = new Intent(MainActivity.this, BookAppointmentActivity.class);
+                startActivity(goPage);
+            }
+        });
+
+        buttonBusinessLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goPage = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(goPage);
+            }
+        });
     }
 }
